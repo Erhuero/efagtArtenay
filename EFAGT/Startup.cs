4131bd886722c5747f0ad79b882d7aa8e6f340ce
Constantin CHTANKO <constantin.chtanko@etu.formation-aftec.com>
2019-11-29 15:11:54 +0100

    Ajoutez des fichiers projet.

---
+++ EFAGT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EFAGT.Startup))]
+namespace EFAGT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
