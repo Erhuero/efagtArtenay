e6321f8832cd7e72e0d9f7281bed9446e9eb4883
Constantin CHTANKO <constantin.chtanko@gmail.com>
2020-01-03 14:02:08 +0100

    Revert "Initialisation"
    This reverts commit 847122829e9ca2f66169b5644eb5c778663da189.

--- EFAGT/Models/Tickets.cs
+++ EFAGT/Models/Tickets.cs
@@ -8,34 +8,20 @@ namespace EFAGT.Models
 {
     public class Tickets
     {
-        [Key]
         [Display(Name = "Numéro du ticket")]
         public int idTicket { get; set; }
-
-        [Display(Name = "Cellule")]
         public string emplacementMaterielTicket{ get; set; }
-
-        [Display(Name = "Type de matériel")]
         public string typeMaterielTicket { get; set; }
-
-        [Display(Name = "Description de l'incident")]
         public string descriptionIncident { get; set; }
 
-        [Display(Name = "Date de creation d'incident")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime dateCreationIncident { get; set; }
 
-        [Display(Name = "Date de cloture")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]              
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime dateClotureIncident { get; set; }
 
-        [Display(Name = "Nom de la cellule")]
         public string nomCellule { get; set; }
-
-        [Display(Name = "Numéro de série")]
         public string numeroSerieMAteriel { get; set; }
-
-        [Display(Name = "Statut")]
         public string etatStatut { get; set; }
     }
 }
--- EFAGT/Models/ViewModele.cs
+++ EFAGT/Models/ViewModele.cs
@@ -8,15 +8,5 @@ namespace EFAGT.Models
     public class ViewModele
     {
         public IEnumerable<Tickets> visualisationTickets { get; set; }//recuperer les données sous forme d'une liste
-        public IEnumerable<Barre> visualisationBarre { get; set; }//recuperer les données sous forme d'une liste
-        public IEnumerable<Cellule> visualisationCellule { get; set; }//recuperer les données sous forme d'une liste
-        public IEnumerable<Materiel> visualisationMateriel { get; set; }//recuperer les données sous forme d'une liste
-        public IEnumerable<NiveauUrgence> visualisationNiveauUrgence { get; set; }//recuperer les données sous forme d'une liste
-        public IEnumerable<Profil> visualisationProfil { get; set; }//recuperer les données sous forme d'une liste
-        public IEnumerable<Statut> visualisationStatut { get; set; }//recuperer les données sous forme d'une liste
-        public IEnumerable<TypeConnexion> visualisationTypeConnexion { get; set; }//recuperer les données sous forme d'une liste
-        public IEnumerable<TypeIP> visualisationTypeIP { get; set; }//recuperer les données sous forme d'une liste
-        public IEnumerable<TypeMateriel> visualisationTypeMateriel { get; set; }//recuperer les données sous forme d'une liste
-        public IEnumerable<Utilisateur> visualisationUtilisateur { get; set; }//recuperer les données sous forme d'une liste
     }
 }
